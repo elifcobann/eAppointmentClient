070ee0623d9a88a95788241ff76eebc29b81c73a
elifcobann <95699826+elifcobann@users.noreply.github.com>
2026-01-26 17:04:32 +0300

    Doctor Create

--- src/app/components/doctors/doctors.component.ts
+++ src/app/components/doctors/doctors.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { asNativeElements, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HttpService } from '../../services/http.service';
 import { DoctorModel } from '../../models/doctor.model';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from "@angular/forms";
+import { departments } from '../../constants';
+import { FormValidateDirective } from 'form-validate-angular';
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule, FormValidateDirective],
   templateUrl: './doctors.component.html',
   styleUrl: './doctors.component.css'
 })
 export class DoctorsComponent implements OnInit {
   doctors: DoctorModel[] = [];
+  departments = departments;
+
+  @ViewChild("addModelCloseBtn") addModelCloseBtn: ElementRef<HTMLButtonElement> | undefined;
+
+  createModel: DoctorModel = new DoctorModel();
 
   constructor(
     private http : HttpService
@@ -24,9 +32,20 @@ export class DoctorsComponent implements OnInit {
   }
 
   getAll() {
-    this.http.post<DoctorModel[]>("Doctor/GetAll", {}, (res)=>{
+    this.http.post<DoctorModel[]>("Doctors/GetAll", {}, (res)=>{
       this.doctors = res.data;
     });
+  }
+
+  add(form: NgForm){
+    if(form.valid){
+      this.http.post<string>("Doctors/Create", this.createModel, (res)=>{
+        console.log(res);
+        this.getAll();
+        this.addModelCloseBtn?.nativeElement.click();
+        this.createModel = new DoctorModel();
+      });
+    }
   }
 
 }
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,72 @@
+import { DepartmentModel } from "./models/doctor.model";
+
 export const api : string = "http://localhost:5012/api"
+
+export const departments : DepartmentModel[] = [
+    {
+        name: "Acil",
+        value: 1
+    },
+    {
+        name: "Radyoloji",
+        value: 2
+    },
+    {
+        name: "Kardiyoloji",
+        value: 3    
+    },
+    {
+        name: "Dermatoloji",
+        value: 4        
+    },
+    {
+        name: "Endokrinoloji",  
+        value: 5      
+    },
+    {   
+        name: "Gastroenteroloji",
+        value: 6        
+    },  
+    {
+        name: "GenelCerrahi",
+        value: 7
+    },
+    {
+        name: "JinekolojiveObstetrik",
+        value: 8    
+    },
+    {
+        name: "Hematoloji", 
+        value: 9
+    },
+    {
+        name: "EnfeksiyonHastalıkları",
+        value: 10
+    },
+    {   
+        name: "Nefroloji",
+        value: 11        
+    },
+    {
+        name: "Nöroloji",
+        value: 12
+    },
+    {
+        name: "Ortopedi",
+        value: 13    
+    },
+    {
+        name: "Pediatri", 
+        value: 14
+    },
+    {
+        name: "Psikiyatri",
+        value: 15
+    },
+    {
+        name: "Pulmonoloji",
+        value: 16        
+    }
+];
+
+ 
